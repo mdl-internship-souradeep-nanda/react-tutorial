1d847672ce1fc33c4dcecfb414c38d2faef903db
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-06 15:16:49 +0530

    Fixed some eslint errors

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "es6": true,
+        "browser": true,
+    },
+    "rules": {
+        // .jsx is legacy according to 
+        // https://github.com/facebook/create-react-app/issues/3052
+        "react/jsx-filename-extension": false,
+    }
+};
