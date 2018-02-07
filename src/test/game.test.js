d63ec4d8b1fb003a7982dba5101ba964804a9138
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-07 11:16:13 +0530

    game test js

--- src/test/game.test.js
+++ src/test/game.test.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import Game from '../game';
 
+configure({ adapter: new Adapter() });
+
 describe('The Game element must have', () => {
-  it('A div with classname game');
-  it('A div with classname game-board');
-  it('A div with classname game-info');
-  it('The component Board');
+  const wrapper = shallow(<Game />);
+  it('A div with classname game', () => {
+    expect(wrapper.find('.game').length).toBe(1);
+  });
+  it('A div with classname game-board', () => {
+    expect(wrapper.find('.game-board').length).toBe(1);
+  });
+  it('A div with classname game-info', () => {
+    expect(wrapper.find('.game-info').length).toBe(1);
+  });
+  it('The component Board', () => {
+    expect(wrapper.find('Board').length).toBe(1);
+  });
 });
