8f556a2d6fb04796260faec89f3b406bbc614f1c
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-07 11:18:58 +0530

    square test added

--- src/test/square.test.js
+++ src/test/square.test.js
@@ -1,7 +1,20 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import Square from '../square';
 
+configure({ adapter: new Adapter() });
+
 describe('The Square component should have', () => {
-  it('A button with className .square');
-  it('A defined onClick Method');
+  const value = 'X';
+  const onClick = jest.fn();
+  const wrapper = shallow(<Square
+    value={value}
+    onClick={onClick}
+  />);
+  it('A button with className .square', () => {
+    expect(wrapper.find('.square').length).toBe(1);
+  });
 });
 
